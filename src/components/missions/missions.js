84a2cd1b14bfcd58791c7882504f8f8c9c229234
tta2yta <tta2yta@gmail.com>
2021-09-07 01:05:45 +0300

    Update missions component

--- src/components/missions/missions.js
+++ src/components/missions/missions.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { getMissionsApi } from "../../redux/missions/actions/missionsAction"
 export const missions=()=>{
+    const missionsLists= useSelector((state)=> state.missions)
+    const dispatch= useDispatch();
+
+    useEffect(()=>{
+        dispatch(getMissionsApi());
+        console.log(missionsLists)
+    }, [])
     return(
         {
-            
+
         }
     )
 }
