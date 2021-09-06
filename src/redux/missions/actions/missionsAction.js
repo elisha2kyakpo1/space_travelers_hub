f5fd7f982b5df748675e6cc876d7e4ae1cbae99c
tta2yta <tta2yta@gmail.com>
2021-09-07 01:20:15 +0300

    Update missionsActions

--- src/redux/missions/actions/missionsAction.js
+++ src/redux/missions/actions/missionsAction.js
@@ -1,16 +1,15 @@
-import * as MISSIONS_API from '../api/missionsApi'
+import * as MISSIONS_API from '../api/missionsApi';
 
-const FETCH_API="FETCH_API"
+const FETCH_API = 'FETCH_API';
 
-
-export const fetchMissions =(payLoad)=>({
-    type:FETCH_API,
-    payLoad:payLoad,
+export const fetchMissions = (payLoad) => ({
+  type: FETCH_API,
+  payLoad,
 });
 
-export const getMissionsApi = ()=>(dispatch)=>{
-    const missions= await MISSIONS_API.getMissions();
-    if (missions){
-        dispatch(missions)
-    }
-}
+export const getMissionsApi = () => async (dispatch) => {
+  const missions = await MISSIONS_API.getMissions();
+  if (missions) {
+    dispatch(missions);
+  }
+};
