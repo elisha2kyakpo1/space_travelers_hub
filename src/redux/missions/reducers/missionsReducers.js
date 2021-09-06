5071682d85797989ba1233031ca49980b61c97be
tta2yta <tta2yta@gmail.com>
2021-09-07 00:49:03 +0300

    Add missionReducers method

--- src/redux/missions/reducers/missionsReducers.js
+++ src/redux/missions/reducers/missionsReducers.js
@@ -1,1 +1,10 @@
+import { act } from "react-dom/cjs/react-dom-test-utils.production.min"
+
 const initialState=[]
+
+const missionsReducer=(state=initialState, action)=>{
+    switch(action.type){
+        case 'FETCH_API':
+            return action.payLoad;
+    }
+}
