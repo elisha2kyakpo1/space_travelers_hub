d4e1ce8d9cfffefdb4aef9efd92e660c76383761
tta2yta <tta2yta@gmail.com>
2021-09-07 23:28:54 +0300

    Update missions component and styles

--- src/components/missions/missions.js
+++ src/components/missions/missions.js
@@ -12,17 +12,52 @@ export const Missions = () => {
     dispatch(getMissionsApi());
     console.log(missionsLists);
   }, []);
+
+  const handleMemberStatus = (id) => (e) => {
+    e.preventDefault();
+    console.log(id);
+    console.log(e.target.id);
+    if (e.target.id === id) {
+      if (e.target.value === 'Join Mission') {
+        e.target.value = 'Leave Mission';
+        document.getElementById(`status-${id}`).innerHTML = 'Active Memebr';
+        document.getElementById(`status-${id}`).className = 'active';
+        document.getElementById(id).className = 'btn-member';
+      } else {
+        e.target.value = 'Join Mission';
+        document.getElementById(`status-${id}`).innerHTML = 'NOT A MEMBER';
+        document.getElementById(`status-${id}`).className = 'not-active';
+        document.getElementById(id).className = 'btn-notmember';
+      }
+    }
+    return true;
+  };
   return (
 
     <div className="missions">
-      {missionsLists.map((item) => (
-        <div key={uuidv4()} className="list-items">
-          <div className="miss-name">{item.mission_name}</div>
-          <div className="miss-desc">{item.description}</div>
-          <div className="status" />
-        </div>
-
-      ))}
+      <div className="list-items header">
+        <div className="miss-name">Mission Name</div>
+        <div className="miss-desc">Description</div>
+        <div className="status-h"> Status</div>
+        <div className="btnjoin" />
+      </div>
+      {missionsLists.map((item) => {
+        const id = uuidv4();
+        return (
+          <div key={id} className="list-items">
+            <div className="miss-name">{item.mission_name}</div>
+            <div className="miss-desc">{item.description}</div>
+            <div className="status">
+              {' '}
+              <span id={`status-${id}`} className="not-active">NOT A MEMBER </span>
+              {' '}
+            </div>
+            <div className="btnjoin">
+              <input type="button" value="Join Mission" className="btn-notmember" onClick={handleMemberStatus(id)} id={id} />
+            </div>
+          </div>
+        );
+      })}
 
     </div>
   );
