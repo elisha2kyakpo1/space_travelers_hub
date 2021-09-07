bd5d0b85cc9f1888dd836800ecf0853e3415acb3
tta2yta <tta2yta@gmail.com>
2021-09-07 23:44:12 +0300

    Add selectedMissions action

--- src/redux/missions/actions/missionsAction.js
+++ src/redux/missions/actions/missionsAction.js
@@ -1,9 +1,14 @@
 import * as MISSIONS_API from '../api/missionsApi';
 
 const FETCH_API = 'FETCH_API';
+const FETCH_API = 'SELECTED_MISSIONS';
 
 export const fetchMissions = (payLoad) => ({
   type: FETCH_API,
+  payLoad,
+});
+export const selectedMissions = (payLoad) => ({
+  type: SELECTED_MISSIONS,
   payLoad,
 });
 
