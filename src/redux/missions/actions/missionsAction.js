4abaef2f0d3d9a1869e5349fd28ec1c5dd7bd30f
tta2yta <tta2yta@gmail.com>
2021-09-09 02:33:01 +0300

    Add action reserve and cancel

--- src/redux/missions/actions/missionsAction.js
+++ src/redux/missions/actions/missionsAction.js
@@ -1,15 +1,21 @@
 import * as MISSIONS_API from '../api/missionsApi';
 
 const FETCH_API = 'FETCH_API';
-const SELECTED_MISSIONS = 'SELECTED_MISSIONS';
+const RESERVE_MISSION = 'RESERVE_MISSION';
+const CANCEL_MISSION = 'CANCEL_MISSION';
 
 export const fetchMissions = (payLoad) => ({
   type: FETCH_API,
   payLoad,
 });
-export const selectedMissions = (payLoad) => ({
-  type: SELECTED_MISSIONS,
-  payLoad,
+const reserveMission = (id) => ({
+  type: RESERVE_MISSION,
+  id,
+});
+
+const cancelMission = (id) => ({
+  type: CANCEL_MISSION,
+  id,
 });
 
 export const getMissionsApi = () => async (dispatch) => {
