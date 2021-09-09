f37c7686e92c10ce58536968da934d6691f1d083
tta2yta <tta2yta@gmail.com>
2021-09-09 11:25:52 +0300

    Update mission components

--- src/components/missions/missions.js
+++ src/components/missions/missions.js
@@ -1,60 +1,51 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { v4 as uuidv4 } from 'uuid';
-import { getMissionsApi } from '../../redux/missions/actions/missionsAction';
+import { getMissionsApi, reserveMission, cancelMission } from '../../redux/missions/actions/missionsAction';
 import './mission.css';
 
 export const Missions = () => {
   const missionsLists = useSelector((state) => state.missions);
+  console.log(missionsLists);
+  // let selectedMissionsItems = [];
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(getMissionsApi());
+    console.log(missionsLists);
   }, []);
 
   const handleMemberStatus = (id) => (e) => {
-    e.preventDefault();
-    if (e.target.id === id) {
-      if (e.target.value === 'Join Mission') {
-        e.target.value = 'Leave Mission';
-        document.getElementById(`status-${id}`).innerHTML = 'Active Memebr';
-        document.getElementById(`status-${id}`).className = 'active';
-        document.getElementById(id).className = 'btn-member';
-      } else {
-        e.target.value = 'Join Mission';
-        document.getElementById(`status-${id}`).innerHTML = 'NOT A MEMBER';
-        document.getElementById(`status-${id}`).className = 'not-active';
-        document.getElementById(id).className = 'btn-notmember';
-      }
-    }
-    return true;
+    if (e.target.value === 'Join Mission') dispatch(reserveMission(id));
+    else dispatch(cancelMission(id));
   };
   return (
 
     <div className="missions">
-      <div className="list-items header">
+      <div className="list-items header-mission">
         <div className="miss-name">Mission Name</div>
         <div className="miss-desc">Description</div>
         <div className="status-h"> Status</div>
         <div className="btnjoin" />
       </div>
-      {missionsLists.map((item) => {
-        const id = uuidv4();
-        return (
-          <div key={id} className="list-items">
-            <div className="miss-name">{item.mission_name}</div>
-            <div className="miss-desc">{item.description}</div>
-            <div className="status">
+      {missionsLists.map(({
+        missionId, missionName, description, reserved,
+      }) => (
+        <div key={missionId} className="list-items">
+          <div className="miss-name">{missionName}</div>
+          <div className="miss-desc">{description}</div>
+          <div className="status">
+            {' '}
+            <span id={`status-${missionId}`} className={reserved ? 'active' : 'not-active'}>
+              { reserved ? 'Active Member' : 'NOT A MEMBER'}
               {' '}
-              <span id={`status-${id}`} className="not-active">NOT A MEMBER </span>
-              {' '}
-            </div>
-            <div className="btnjoin">
-              <input type="button" value="Join Mission" className="btn-notmember" onClick={handleMemberStatus(id)} id={id} />
-            </div>
+            </span>
+            {' '}
           </div>
-        );
-      })}
+          <div className="btnjoin">
+            <input type="button" value={reserved ? 'Leave Mission' : 'Join Mission'} className={reserved ? 'btn-member' : 'btn-notmember'} onClick={handleMemberStatus(missionId)} />
+          </div>
+        </div>
+      ))}
 
     </div>
   );
